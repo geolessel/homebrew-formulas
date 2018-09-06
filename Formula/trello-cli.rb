a9ac41a450bc73acca29c0a8ea482bcf80292c21
Geoffrey Lessel <geo@geoffreylessel.com>
2018-09-06 08:28:51 -0700

    trello-cli release v0.2.0

--- Formula/trello-cli.rb
+++ Formula/trello-cli.rb
@@ -1,19 +1,19 @@
 class TrelloCli < Formula
   desc "A Trello CLI app using ncurses. It's Trello, but snappy!"
-  homepage "https://github.com/geolessel/trello-cli"
-  url "https://github.com/geolessel/trello-cli/archive/v0.1.2.tar.gz"
-  sha256 "f66963b82bd3518d0ca5a52b8316cef33bb70b1bde27a969f861e27cdd81e54d"
-  depends_on "crystal" => :build
-  depends_on "ncurses"
+  homepage 'https://github.com/geolessel/trello-cli'
+  url 'https://github.com/geolessel/trello-cli/archive/v0.2.0.tar.gz'
+  sha256 '8a826934280c5efdbc72a2abcaaa63e02c7e0b0ed9a432ab8614325bd73902bf'
+  depends_on 'crystal' => :build
+  depends_on 'ncurses'
 
-  head "https://github.com/geolessel/trello-cli.git"
+  head 'https://github.com/geolessel/trello-cli.git'
 
   def install
-    system "make"
-    bin.install "bin/trello"
+    system 'make'
+    bin.install 'bin/trello'
   end
 
   test do
-    system "false"
+    system 'false'
   end
 end
