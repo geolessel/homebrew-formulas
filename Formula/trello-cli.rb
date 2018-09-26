49361dda935d56a1d943448d5d4527d650e69ac1
Geoffrey Lessel <geo@geoffreylessel.com>
2018-09-26 09:28:26 -0700

    Update trello-cli to require openssl for build

--- Formula/trello-cli.rb
+++ Formula/trello-cli.rb
@@ -4,12 +4,13 @@ class TrelloCli < Formula
   url 'https://github.com/geolessel/trello-cli/archive/v0.2.1.tar.gz'
   sha256 '3807b7c4a54877ea54cdb70480b5a2d28abae84ac7f28b35f80e41c73d7857dc'
   depends_on 'crystal' => :build
+  depends_on 'openssl' => :build
   depends_on 'ncurses'
 
   head 'https://github.com/geolessel/trello-cli.git'
 
   def install
-    system 'make'
+    system 'PATH="/usr/local/opt/openssl/bin:$PATH" LDFLAGS="-L/usr/local/opt/openssl/lib" CPPFLAGS="-I/usr/local/opt/openssl/include" PKG_CONFIG_PATH="/usr/local/opt/openssl/lib/pkgconfig" make'
     bin.install 'bin/trello'
   end
 
